43feb10b287d3cb2f163592313235362a73c2801
fahmirumagutawan <fahmigutawan@gmail.com>
2022-05-18 16:13:06 +0700

    add new services method

--- src/main/java/com/example/ubkasirjavafx/services/Services.java
+++ src/main/java/com/example/ubkasirjavafx/services/Services.java
@@ -1,8 +1,20 @@
 package com.example.ubkasirjavafx.services;
 
+import javafx.animation.*;
 import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
-public class Delay {
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Services {
     public void postDelayed(Runnable toRun, long wait) {
         Thread thread = new Thread();
 
@@ -23,5 +35,12 @@ public class Delay {
         // Daemon - allows the thread to be terminated when JVM terminates
         thread.setDaemon(true);
         thread.start();
+    }
+
+    public void transferScene(String fxmlName, Window window) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlName));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Stage stage = (Stage) window;
+        stage.setScene(scene);
     }
 }
--- src/main/java/com/example/ubkasirjavafx/ui/SceneController.java
+++ src/main/java/com/example/ubkasirjavafx/ui/SceneController.java
@@ -1,6 +1,6 @@
 package com.example.ubkasirjavafx.ui;
 
-import com.example.ubkasirjavafx.services.Delay;
+import com.example.ubkasirjavafx.services.Services;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -45,20 +46,18 @@ public class SceneController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         /*SPLASH SCREEN*/
-        new Delay().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(getClass().getResource("/com/example/ubkasirjavafx/homemenu_activity.fxml"));
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/ubkasirjavafx/homemenu_activity.fxml"));
-                try {
-                    Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-                    Stage stage = (Stage) splashscreen_icon_iv.getScene().getWindow();
-                    stage.setScene(scene);
-                } catch (IOException e) {
-                    e.printStackTrace();
+        new Services().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            new Services().transferScene("/com/example/ubkasirjavafx/homemenu_activity.fxml", splashscreen_icon_iv.getScene().getWindow());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
-            }
-        }, 2000);
+                , 2000);
     }
     /**==[END]==*/
 }
