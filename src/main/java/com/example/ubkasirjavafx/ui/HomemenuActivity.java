5597a790fb632dd7b9e7f6b68806aabfe7df3b0f
HanifRobby <96159381+HanifRobby@users.noreply.github.com>
2022-05-18 14:38:45 +0700

    New layout has been added

---
+++ src/main/java/com/example/ubkasirjavafx/ui/HomemenuActivity.java
@@ -0,0 +1,23 @@
+package com.example.ubkasirjavafx.ui;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomemenuActivity {
+
+    @FXML
+    private Button btnKembali;
+
+    public HomemenuActivity() {
+
+
+    }
+
+}
